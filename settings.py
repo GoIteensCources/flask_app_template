27db6a57bfbb088a72432e7eef8a1f8e5b696887
Valentin <valoliinykov.work@gmail.com>
2024-06-27 10:44:21 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -5,3 +5,4 @@ class Config:
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = f'sqlite:///db_{BASE_DIR.split("/")[-1]}.db'
+    SECRET_KEY = 'your secret key'
